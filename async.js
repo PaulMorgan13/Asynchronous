ad3470bc5f5be5d1281b66a53bd88c62f20c501b
Paul Morgan <orbitingmind@gmail.com>
2021-07-03 14:10:39 -0400

    promises

--- async.js
+++ async.js
@@ -1,74 +1,89 @@
-let  iventory = {
+//const { reject } = require("async");
+
+let  inventory = {
         fruit : ["strawberry", "grapes", "banana", "apple"],
         liquid : ["water", "soda", "juice", "shake"],
         holder : ["cone", "cup", "bowl", "stick"]  
 
 }
 
+ 
+
+let isShopOpen = true; 
 
 
 
-iventory.fruit[0];   
-iventory.liquid[0];  
-
-console.log(iventory.fruit[0])
-console.log(iventory.liquid[0])
-console.log(iventory.holder[0])
-
-
-
-
-
-let order = (Fruit_name, call_pro) =>{
-
-    setTimeout(()=>{
-        console.log(`${iventory.fruit[Fruit_name]} was selected`)    
-
-        production()
-    },2000)
-    call_pro()
-};   
-
-
-let production =() =>{   
-    console.log("productions has started")       
-
-    setTimeout(()=>{
-        console.log(`fruit has been chopped`);  
-
-        setTimeout(()=>{
-                console.log(`${iventory.liquid[0]} and ${iventory.liquid[2]} was added`) 
+let order = (time, work) =>{ 
+        return new Promise( (resolve, reject) =>{
+            if (isShopOpen){ 
 
                 setTimeout(()=>{
-                        console.log("machine has started");  
-                            setTimeout(()=>{
-                                console.log(`the juice was filled in the ${iventory[1]}`)   
-
-                                    setTimeout(()=>{ 
-
-                                                setTimeout(()=>{
-                                                    console.log("icing was served")
-                                                },  1000)
-                                        console.log("there was no topping to put on")
-                                    }, 2000) 
-
-                            }, 1000)
-                }, 1000)
-        },1000)
-
-    }, 2000)
+                    resolve(work())
+                },time)
+            }
+            else {
+                reject(console.log("our shop is closed "))
+            }
 
 
 
-
-    setTimeout(()=>{
-
-    },0000)
-
-};   
-
-
-order(2, production)
+        })
 
 
 
+}
+
+
+order(2000,()=> console.log(`${inventory.fruit[0]} was selected`))
+    
+    .then(()=>{
+        return order(0000, ()=>console.log('production has started'))
+    }) 
+
+    .then( 
+        ()=>{
+            return order(2000, ()=> console.log(`cutting ${inventory.fruit[0]}`))
+        }
+    ) 
+
+    .then(()=> {
+        return order(1000, ()=> {
+            console.log(`${inventory.liquid[0]} and ${inventory.liquid[1]} was selected.`)
+        })
+    })  
+
+    .then(()=>{
+        return order(1000, ()=>{
+            console.log("start the machine")
+        })
+    })  
+
+    .then(()=>{
+        return order(2000, ()=>{
+            console.log(`ice cream was placed in the ${inventory.holder[1]}`)
+        })
+    })  
+
+    .then(()=>{
+        return order(3000, ()=>{
+            console.log(`${inventory.fruit[3]} was selected` )
+        })
+    })  
+
+    .then(()=>{
+        return order(1000, ()=>{
+            console.log("your order was served")
+        })
+    })    
+
+
+    .catch(()=>{
+        console.log("customer left")
+
+    })  
+
+
+    .finally(()=>{
+        console.log("day ended")
+
+    })
